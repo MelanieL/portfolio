7b6ba4cc2b9b97c4c010fa891df6b9200781ef45
Melanie Phillips <melanieleeking@gmail.com>
2018-03-14 12:36:52 -0400

    Responsive styling complete.

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -4,6 +4,24 @@ var app = {};
 
 app.events = function () {
     $('a').smoothScroll();
+    // $.scrollify({
+    //     section: ".snap",
+    //     sectionName: "snap",
+    //     interstitialSection: "",
+    //     easing: "easeOutExpo",
+    //     scrollSpeed: 1100,
+    //     offset: 0,
+    //     scrollbars: true,
+    //     standardScrollElements: "",
+    //     setHeights: true,
+    //     overflowScroll: true,
+    //     updateHash: true,
+    //     touchScroll: true,
+    //     before: function () { },
+    //     after: function () { },
+    //     afterResize: function () { },
+    //     afterRender: function () { }
+    // });
 };
 
 app.init = function () {
